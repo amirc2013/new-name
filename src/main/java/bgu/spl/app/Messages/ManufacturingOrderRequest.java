82b539f8e981b5cca3b45e7f91cfa68b35ccb820
matantsu <matantsuberi@gmail.com>
2015-12-22 21:59:21 +0200

    finished messages

--- src/main/java/bgu/spl/app/Messages/ManufacturingOrderRequest.java
+++ src/main/java/bgu/spl/app/Messages/ManufacturingOrderRequest.java
@@ -1,9 +1,20 @@
 package bgu.spl.app.Messages;
 
+import bgu.spl.app.Passive.Receipt;
 import bgu.spl.mics.Message;
+import bgu.spl.mics.Request;
 
 /**
  * Created by Amir on 22/12/2015.
  */
-public class ManufacturingOrderRequest implements Message {
+public class ManufacturingOrderRequest implements Request<Receipt> {
+    protected String shoeType;
+
+    public ManufacturingOrderRequest(String shoeType) {
+        this.shoeType = shoeType;
+    }
+
+    public String getShoeType() {
+        return shoeType;
+    }
 }
